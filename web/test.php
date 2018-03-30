b16d8e5f6c7acbc549cdb8032bca4e33dfaa33f6
lafitewu <863512936@qq.com>
2018-03-30 14:04:21 +0800

    no message

--- web/test.php
+++ web/test.php
@@ -7,5 +7,5 @@
  
 	$mysql_database='uesr'; //改成自己的mysql数据库名
 	$conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("error connecting") ; //连接数据库
-	echo "双击666";
+	echo "双击6667";
 ?>
