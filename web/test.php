c4b3c4ec61fffd6a83939738e262792fb644e98e
lafitewu <863512936@qq.com>
2018-03-30 14:02:15 +0800

    no message

--- web/test.php
+++ web/test.php
@@ -1,5 +1,11 @@
 <?php
-	// require("comm/init.php");  
-	// $conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("error connecting") ; //连接数据库
+	$mysql_server_name='den1.mysql6.gear.host'; //改成自己的mysql数据库服务器
+ 
+	$mysql_username='lafitedata'; //改成自己的mysql数据库用户名
+ 
+	$mysql_password='950817dsz!'; //改成自己的mysql数据库密码
+ 
+	$mysql_database='uesr'; //改成自己的mysql数据库名
+	$conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("error connecting") ; //连接数据库
 	echo "双击666";
 ?>
