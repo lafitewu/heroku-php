bf9df63c564beabec9f21e6308b7e3b772f5a12b
lafitewu <863512936@qq.com>
2018-03-30 13:47:21 +0800

    测试mysql

---
+++ web/comm/init.php
@@ -0,0 +1,10 @@
+<?php  
+	$mysql_server_name='den1.mysql6.gear.host'; //改成自己的mysql数据库服务器
+ 
+	$mysql_username='lafitedata'; //改成自己的mysql数据库用户名
+ 
+	$mysql_password='950817dsz!'; //改成自己的mysql数据库密码
+ 
+	$mysql_database='uesr'; //改成自己的mysql数据库名
+
+?>
---
+++ web/test.php
@@ -0,0 +1,4 @@
+<?php
+	require("comm/init.php");  
+	$conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("error connecting") ; //连接数据库
+?>
