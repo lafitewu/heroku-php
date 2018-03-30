6347fae2680d3c3a74393fe405c095945abb8858
lafitewu <863512936@qq.com>
2018-03-30 14:04:51 +0800

    no message

--- web/test.php
+++ web/test.php
@@ -1,11 +1,11 @@
 <?php
-	$mysql_server_name='den1.mysql6.gear.host'; //改成自己的mysql数据库服务器
+	// $mysql_server_name='den1.mysql6.gear.host'; //改成自己的mysql数据库服务器
  
-	$mysql_username='lafitedata'; //改成自己的mysql数据库用户名
+	// $mysql_username='lafitedata'; //改成自己的mysql数据库用户名
  
-	$mysql_password='950817dsz!'; //改成自己的mysql数据库密码
+	// $mysql_password='950817dsz!'; //改成自己的mysql数据库密码
  
-	$mysql_database='uesr'; //改成自己的mysql数据库名
-	$conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("error connecting") ; //连接数据库
+	// $mysql_database='uesr'; //改成自己的mysql数据库名
+	// $conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("error connecting") ; //连接数据库
 	echo "双击6667";
 ?>
