39a5b9a4be8eb6d2692a31e217603e0f9502a64e
lafitewu <863512936@qq.com>
2018-03-30 14:21:04 +0800

    no message

--- web/test.php
+++ web/test.php
@@ -7,5 +7,24 @@
  
 	// $mysql_database='uesr'; //改成自己的mysql数据库名
 	// $conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("error connecting") ; //连接数据库
+
+  error_reporting(E_ALL);
+
+  error_reporting(E_ALL & ~E_NOTICE);
+
+
+
+  $link = mysql_connect('den1.mysql6.gear.host', 'lafitedata', '950817dsz');
+
+  if (!$link) {
+
+    die('Could not connect: ' . mysql_error()); }
+
+  else{
+
+    echo 'Connected successfully';  }
+
+    mysql_close($link);
+
 	echo "双击6667";
 ?>
