c1fced4ef8618f7158a63fc3fa802bda97e943d4
lafitewu <863512936@qq.com>
2018-03-30 13:58:46 +0800

    no message

--- web/test.php
+++ web/test.php
@@ -1,4 +1,5 @@
 <?php
-	require("comm/init.php");  
-	$conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("error connecting") ; //连接数据库
+	// require("comm/init.php");  
+	// $conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("error connecting") ; //连接数据库
+	echo "双击666";
 ?>
